6e053c67cfc8436eca2a8058dd6ca5302e4d37c5
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2022-11-17 09:43:39 +0300

    controllers added

---
+++ src/main/java/ru/project/hibernateJpa/controller/BookController.java
@@ -0,0 +1,123 @@
+package ru.project.hibernateJpa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.project.hibernateJpa.model.Book;
+import ru.project.hibernateJpa.model.Person;
+import ru.project.hibernateJpa.service.BookService;
+import ru.project.hibernateJpa.service.PeopleService;
+import ru.project.hibernateJpa.utils.BookValidator;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+//это контроллер
+@RequestMapping("/books")
+//дефолтный запрос
+public class BookController {
+    private final BookService bookService;
+    private final PeopleService peopleService;
+    private final BookValidator bookValidator;
+
+    @Autowired
+    public BookController(BookService bookService, PeopleService peopleService, BookValidator bookValidator) {
+        this.bookService = bookService;
+        this.peopleService = peopleService;
+        this.bookValidator = bookValidator;
+    }
+
+    @GetMapping
+    /*получаем по какому запросу, сейчас по дефолтному*/
+    public String showAll(Model model) {
+        model.addAttribute("books"/*как обратиться на странице отображения*/,
+                bookService.findAll())/*что получим при обращении*/;
+        return "books/all";
+        /*на какую страницу переход*/
+    }
+
+    @GetMapping("/{id}")
+    /*получаем по какому запросу, сейчас по id*/
+    public String showOne(@PathVariable("id") /*обращение может меняться*/ int id, Model model,
+                          @ModelAttribute("person"/*какую переменную внедрить на страницу отобраения*/)
+                          Person person /*что внедряем*/) {
+        model.addAttribute("book", bookService.findOne(id));
+
+        Optional<Person> bookOwner = bookService.getBookOwner(id);
+
+        if (bookOwner.isPresent()) {
+            model.addAttribute("owner", bookOwner.get());
+            //если у книга у человека, показываем его
+        } else {
+            model.addAttribute("people", peopleService.findAll());
+            //если нет, то передаем список людей, для выбора из списка
+        }
+
+        return "books/one";
+    }
+
+    @GetMapping("/new")
+    /*получаем*/
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "/books/new";
+    }
+
+    @PostMapping()
+    /*добавляем на сервере*/
+    public String create(@ModelAttribute("book") @Valid Book book,
+                         BindingResult bindingResult/*если будет ошибка, она поместиться сюда*/) {
+
+        bookValidator.validate(book, bindingResult);
+
+        if (bindingResult.hasErrors()) {//если есть ошибка будет выполнено...
+            return "books/new";
+        }
+
+        bookService.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("book", bookService.findOne(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    /*изменяем на сервере*/
+    public String update(@ModelAttribute("book") @Valid Book book,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+
+        bookValidator.validate(book, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "books/edit";
+        }
+
+        bookService.update(id, book);
+        return "redirect:/books";
+    }
+
+    @DeleteMapping("/{id}")
+    /*удаялем на сервере*/
+    public String delete(@PathVariable("id") int id) {
+        bookService.delete(id);
+        return "redirect:/books";
+    }
+
+    @PatchMapping("/{id}/release")
+    public String release(@PathVariable("id") int id) {
+        bookService.release(id);
+        return "redirect:/books/" + id;
+    }
+
+    @PatchMapping("/{id}/assign")
+    public String assign(@PathVariable("id") int id, @ModelAttribute("person") Person selectedPerson) {
+        bookService.assign(id, selectedPerson);
+        return "redirect:/books/" + id;
+    }
+}
---
+++ src/main/java/ru/project/hibernateJpa/controller/PeopleController.java
@@ -0,0 +1,87 @@
+package ru.project.hibernateJpa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.project.hibernateJpa.model.Person;
+import ru.project.hibernateJpa.service.PeopleService;
+import ru.project.hibernateJpa.utils.PersonValidator;
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PeopleService peopleService;
+    private final PersonValidator personValidator;
+
+    @Autowired
+    public PeopleController(PeopleService peopleService, PersonValidator personValidator) {
+        this.peopleService = peopleService;
+        this.personValidator = personValidator;
+    }
+
+    @GetMapping()
+    public String showAll(Model model) {
+        model.addAttribute("people", peopleService.findAll());
+        return "people/all";
+    }
+
+    @GetMapping("/{id}")
+    public String showOne(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", peopleService.findOne(id));
+        model.addAttribute("books", peopleService.getBooksByPersonId(id));//при отображении человека нужно же ще получить его список книг
+
+        return "people/one";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+
+        personValidator.validate(person, bindingResult); //проверка по кастомному валидатору
+
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
+
+        peopleService.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", peopleService.findOne(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+
+        personValidator.validate(person, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+
+        peopleService.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        peopleService.delete(id);
+        return "redirect:/people";
+    }
+}
